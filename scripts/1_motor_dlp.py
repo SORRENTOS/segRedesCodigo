4c6bb67640a813aeb4e9fd39396f212bc5b86fc0
SORRENTOS <benjamin.rodriguezso@usm.cl>
2026-04-03 17:09:54 +0000

    Initial clean commit without secrets

---
+++ scripts/1_motor_dlp.py
@@ -0,0 +1,5 @@
+import re, json
+def ofuscar_datos(log_crudo):
+    pass # TODO: Implementar lógica Ley 21.719
+if __name__ == "__main__":
+    print("Módulo DLP iniciado.")
---
+++ scripts/2_cliente_api.py
@@ -0,0 +1,3 @@
+import aiohttp, asyncio, os
+from dotenv import load_dotenv
+# TODO: Conexión asíncrona a la API.
